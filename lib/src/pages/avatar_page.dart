d0871bc4d2689a7098a48b0dc329ce6d8de1e3b1
Flores45 <estrella_yani_13@hotmail.com>
2022-12-08 13:37:34 -0500

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,35 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Avatar Page"),),
+      appBar: AppBar(
+        title: Text("Avatar Page"),
+        actions:  <Widget>  [
+          Container(
+            padding:  EdgeInsets.all(5.0 ),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://yt3.ggpht.com/ytc/AMLnZu_DRiYS6n49aEeTmFYsAlzix26g_4SoltvMmh_Fuw=s900-c-k-c0x00ffffff-no-rj'),
+              radius:25 ,
+            ),
+          ),
+
+
+          Container(
+            margin: EdgeInsets.only(right:  20),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.indigo,
+            ),
+          ),
+        ],
+
+        ),
+        body: Center(
+          child: FadeInImage(
+            image: NetworkImage('https://i.ytimg.com/vi/qAbK0pTV_dg/maxresdefault.jpg') ,
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 1000),
+          ),
+          ),
     );
   }
 }
